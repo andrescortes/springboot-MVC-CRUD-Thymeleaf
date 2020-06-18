bef38e82eef78bcc204a3b352a27e32be29dff85
andrescortes <andresabyac@gmail.com>
2020-06-18 09:15:10 -0500

    Implementation MVC

---
+++ src/test/java/co/edu/utp/maffu/pago/PagoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.maffu.pago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
